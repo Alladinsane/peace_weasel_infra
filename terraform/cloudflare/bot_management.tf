6ade840aafc6fdcbb22f00e75b28422e24871a15
Todd Meder <toddbmeder@gmail.com>
2026-09-07 09:44:03 -0400

    fix: bot management breaks stripe integration

--- terraform/cloudflare/bot_management.tf
+++
@@ -1,8 +0,0 @@
-# Bot Fight Mode is available on the Cloudflare Free plan. It challenges
-# likely automated traffic before it reaches the OCI origin and does not use
-# one of the five Custom Rules slots in waf.tf.
-resource "cloudflare_bot_management" "this" {
-  zone_id    = var.zone_id
-  enable_js  = true
-  fight_mode = true
-}
